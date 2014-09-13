343a27370b68c2b540687b23340b4006004ba173
Zaven Muradyan <zaven@skepsi.me>
2014-09-12 17:52:31 -0700

    Sync raw with jekyll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,70 @@
 (function($) {
     'use strict';
 
+    var $window = $(window);
     var $body = $('html, body');
+    var $main = $('#main');
+
+    // Add scroll trigger
+    var $scrollButton = $('.scroll-button');
+
+    (function() {
+        var initialComplete = false;
+        var topOffset = -150;
+        var leftOffset = 75;
+        var animDuration = 300;
+
+        var scrollStart = 400;
+        var lastScroll = 0;
+
+        function calculatePosition(scroll) {
+            var mainWidth = $main.width();
+            var topPos = topOffset + $window.height();
+            var leftPos = leftOffset + mainWidth + ($body.width() - mainWidth) / 2
+
+            return {
+                top: scroll + topPos,
+                left: leftPos
+            };
+        }
+
+        $window.scroll(function() {
+            var scroll = $window.scrollTop();
+
+            // Only change the class when the scroll point is passed
+            if (scroll > scrollStart && lastScroll <= scrollStart ) {
+                $scrollButton.addClass('active');
+            } else if (scroll <= scrollStart && lastScroll > scrollStart) {
+                $scrollButton.removeClass('active');
+            }
+
+            var pos = calculatePosition(scroll);
+
+            // Set the CSS initially, to avoid an animation when the page loads
+            if (!initialComplete) {
+                $scrollButton.css(pos);
+                initialComplete = true;
+            } else {
+                $scrollButton.animate(pos, { duration: animDuration, queue: false });
+            }
+
+            lastScroll = scroll;
+        });
+
+        $window.resize(function() {
+            var scroll = $window.scrollTop();
+            $scrollButton.css(calculatePosition(scroll));
+        });
+    })();
+
+    // Add up-scroll handler
+    $scrollButton.find('a').click(function(event) {
+        event.preventDefault();
+        $body.animate({ scrollTop: 0 });
+    });
 
     // Add smooth page transitions
-    var $content = $('#main').smoothState({
+    var $content = $main.smoothState({
         development: true,
         prefetch: true,
         pageCacheSize: 4,
