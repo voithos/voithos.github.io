86300d9b0ca34288e49a89935747e7df6ac2a252
Zaven Muradyan <zaven@voithos.io>
2020-04-19 20:34:10 -0700

    Add deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
-# TODO: Fill this out
 
+# Exit on errors.
+set -e
+
+# First clone the source repo so that we don't affect the current checked out
+# state.
+repo_dir=$(pwd)
+clone_dir=$(mktemp -d -t voithos-clone-XXXXXXXXXX)
+tmp_dir=$(mktemp -d -t voithos-raw-XXXXXXXXXX)
+
+echo "Beginning clone to ${clone_dir}"
+pushd "${clone_dir}" > /dev/null
+git clone -q "${repo_dir}" .
+git checkout "source"
+
+echo "Beginning deploy to ${tmp_dir}"
+hugo -d "${tmp_dir}"
+
+echo "Copying to master"
+git checkout master
+rsync -az --delete --filter='P .git' "${tmp_dir}/" .
+git add -A
+git commit -m "Sync raw from source."
+git push "${repo_dir}" master
+
+echo "Done"
+popd > /dev/null
+rm -rf "${tmp_dir}" "${clone_dir}"
