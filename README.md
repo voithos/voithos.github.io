a12b4d88b01141c7b30b3a2b66888e19b56f047e
Zaven Muradyan <zaven@voithos.io>
2021-05-24 21:23:24 -0700

    Add basic placeholder background.

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Personal site of Zaven Muradyan.
 
 ### Requirements
 hugo v0.83
+node v14.17.0
 
 ### Deployment
 `./deploy.sh` - Automatically build and deploy the current site in a raw form
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,38 @@
 import * as THREE from 'three';
 
-console.log(THREE);
+import {WEBGL} from 'three/examples/jsm/WebGL.js';
+
+function createBackdrop() {
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+      90, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const renderer = new THREE.WebGLRenderer({antialias : true});
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  // Add to body.
+  renderer.domElement.className = 'backdrop';
+  document.body.appendChild(renderer.domElement);
+
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshBasicMaterial({color : 0x00ff00});
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  camera.position.z = 5;
+
+  function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+  }
+  animate();
+}
+
+if (WEBGL.isWebGLAvailable()) {
+  createBackdrop();
+} else {
+  console.warn(
+      'Browser does not support WebGL; defaulting to static background');
+}
